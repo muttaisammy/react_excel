a6a87b6d7fa9f71a73079db40ad8f981928b0d14
Sam Muttai <42063584+muttaisammy@users.noreply.github.com>
2024-01-25 11:30:46 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HomeComponent from "./components/home.component";
+
+function App() {
+  return (
+    <main> 
+        <HomeComponent /> 
+    </main>
+  );
+}
+
+export default App;
