a030dd2d1587f73407ec5ac9d5e64818492f517d
Sam Muttai <42063584+muttaisammy@users.noreply.github.com>
2024-01-25 12:23:08 +0300

    2nd

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { read, utils, writeFile } from 'xlsx';
 
 const HomeComponent = () => {
-    const [movies, setMovies] = useState([]);
+    const [scores, setScores] = useState([]);
 
     const handleImport = ($event) => {
         const files = $event.target.files;
@@ -15,7 +15,7 @@ const HomeComponent = () => {
 
                 if (sheets.length) {
                     const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
-                    setMovies(rows)
+                    setScores(rows)
                 }
             }
             reader.readAsArrayBuffer(file);
@@ -24,17 +24,21 @@ const HomeComponent = () => {
 
     const handleExport = () => {
         const headings = [[
-            'Movie',
-            'Category',
-            'Director',
-            'Rating'
+            'County',
+            'Facility',
+            'Assessment_Date',
+            'Assessment_Lead',
+            'SET',
+            'CEE',
+            'Score',
+            'Comments'
         ]];
         const wb = utils.book_new();
         const ws = utils.json_to_sheet([]);
         utils.sheet_add_aoa(ws, headings);
-        utils.sheet_add_json(ws, movies, { origin: 'A2', skipHeader: true });
+        utils.sheet_add_json(ws, scores, { origin: 'A2', skipHeader: true });
         utils.book_append_sheet(wb, ws, 'Report');
-        writeFile(wb, 'Movie Report.xlsx');
+        writeFile(wb, 'movies.xlsx');
     }
 
     return (
@@ -64,29 +68,37 @@ const HomeComponent = () => {
                     <table className="table">
                         <thead>
                             <tr>
-                                <th scope="col">Id</th>
-                                <th scope="col">Movie</th>
-                                <th scope="col">Category</th>
-                                <th scope="col">Director</th>
-                                <th scope="col">Rating</th>
+                                <th scope="col">County</th>
+                                <th scope="col">Facility</th>
+                                <th scope="col">Assessment_Date</th>
+                                <th scope="col">Assessment_Lead</th>
+                                <th scope="col">SET</th>
+                                <th scope="col">CEE</th>
+                                <th scope="col">Score</th>
+                                <th scope="col">Comments</th>
+                                
                             </tr>
                         </thead>
                         <tbody> 
                                 {
-                                    movies.length
+                                    scores.length
                                     ?
-                                    movies.map((movie, index) => (
+                                    scores.map((score, index) => (
                                         <tr key={index}>
                                             <th scope="row">{ index + 1 }</th>
-                                            <td>{ movie.Movie }</td>
-                                            <td>{ movie.Category }</td>
-                                            <td>{ movie.Director }</td>
-                                            <td><span className="badge bg-warning text-dark">{ movie.Rating }</span></td>
+                                            <td>{ score.County }</td>
+                                            <td>{ score.Facility }</td>
+                                            <td>{ score.Assessment_Date }</td>
+                                            <td>{ score.Assessment_Lead }</td>
+                                            <td>{ score.SET }</td>
+                                            <td>{ score.CEE }</td>
+                                            <td>{ score.Score }</td>
+                                            <td><span className="badge bg-warning text-dark">{ score.Comments }</span></td>
                                         </tr> 
                                     ))
                                     :
                                     <tr>
-                                        <td colSpan="5" className="text-center">No Movies Found.</td>
+                                        <td colSpan="5" className="text-center">No Reports Found.</td>
                                     </tr> 
                                 }
                         </tbody>
